785415f316b05c684e1aa121f5233f69eee62ed9
Mojtaba Kaviani <mojtaba.kaviani@outlook.com>
2022-02-27 12:36:40 +0330

    support visual studio 2022

--- SqlTools/Properties/AssemblyInfo.cs
+++ SqlTools/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kaviansoft")]
 [assembly: AssemblyProduct("SqlTools")]
-[assembly: AssemblyCopyright("Copyright 2020 © Kaviansoft")]
+[assembly: AssemblyCopyright("Copyright 2022 © Kaviansoft")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.2.0")]
-[assembly: AssemblyFileVersion("2.1.2.0")]
+[assembly: AssemblyVersion("2.1.4.0")]
+[assembly: AssemblyFileVersion("2.1.4.0")]
